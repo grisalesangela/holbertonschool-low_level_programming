03188d26baf52d5d6c8a91ee15045d35d71999c0
grisalesangela <angelagrisales1@gmail.com>
2021-10-02 22:55:29 -0500

    :lipstick: Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit = 0;
+
+	char ch;
+	for (digit = 0; digit <10; digit++)
+	{
+		putchar((digit % 10) + '0');
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
