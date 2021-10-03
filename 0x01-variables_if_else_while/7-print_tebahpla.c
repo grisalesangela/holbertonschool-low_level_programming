f75b4a0851f7b7b44c27f43c66a5f0273f45375c
grisalesangela <angelagrisales1@gmail.com>
2021-10-02 22:36:30 -0500

    :rocket: Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar ('\n');
+	return (0);
+}	
