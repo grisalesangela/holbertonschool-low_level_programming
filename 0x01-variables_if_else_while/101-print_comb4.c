afd9419391b2d7e6b37b9cf2f47cc605b7211ab4
grisalesangela <angelagrisales1@gmail.com>
2021-10-10 21:04:28 -0500

    :lock: The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints combinations of 3 different digits
+ * return: Always 0
+ */
+
+int main (void)
+{
+	int a;
+	int n;
+	int g;
+	
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (n = '1'; n <= '9'; n++)
+		{
+			for (g = '1'; g <= '9'; g++)
+			{
+				if (a < n && n < g)
+				{
+					putchar(a);
+					putchar(n);
+					putchar(g);
+					if (a == '7' && n == '8' && g == '9')
+					{
+						putchar(10);
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
