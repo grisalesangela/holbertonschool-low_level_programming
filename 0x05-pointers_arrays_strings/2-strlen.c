28e16eb7f1f3048a31819553d3ad0dac8386f114
grisalesangela <angelagrisales1@gmail.com>
2021-10-11 22:13:46 -0500

    :fire: This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to count
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
