401c04d1069ba7384a544aa04015ae74f40132db
grisalesangela <angelagrisales1@gmail.com>
2021-10-11 20:39:05 -0500

    :art: 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar - check the code 
+ * @c: character to print
+ * Return: Always 0.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
