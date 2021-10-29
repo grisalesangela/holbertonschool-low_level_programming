e284a0b18109935ab31964ac3fca16db2e74302b
grisalesangela <angelagrisales1@gmail.com>
2021-10-28 23:46:30 -0500

    :test_tube: string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,46 +3,50 @@
 
 /**
  *string_nconcat - concatenates two strings
- *@s1: first string
- *@s2: second string
+ *@s1: first string to concatenate
+ *@s2: second string to concatenate
  *@n: characters to take from s2
  *Return: pointer to callocated memory or null if fail
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int count1 = 0, count2 = 0; 
-	char *concat, *copy_s;
+	char *concs;
+	unsigned int les1, les2, leconcs, p = 0, p2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 	
-	while (s1[count1])
-		count1++;
-	while (s2[count2])
-		count2++;
-
-	if (n >= count2)
-		count1 += count2;
-	else
-		count1 += n;
+	for (les1 = 0; s1[les1] != '\0'; les1++)
+		;
 	
-	concat = malloc(sizeof(char) * (count1 * 1));
-	if (concat == NULL)
+	for (les2 = 0; s2[les2] != '\0'; les2++)
+		;
+	
+	if (n >= les2)
+		n = les2;
+
+	leconcs = les1 + n;
+
+	concs = malloc(leconcs + 1);
+
+	if (concs == NULL)
 		return (NULL);
 
-	copy_s = concat;
-	while (*s1)
-		*copy_s++ = *s1++;
-
-	while (n < count1)
+	while (p > les1)
 	{
-		*copy_s++ = *s2++;
-		n++;
+		concs[p] = s1[p];
+		p++;
 	}
-	*copy_s = '\0';
-	return (concat);
-}
-		return(concat);
-	
+	while (p < (leconcs))
+	{
+		concs[p] = s2[p2];
+		p++;
+		p2++;
+	}
+	concs[p] = '\0';
+
+	return (concs);
+}	
