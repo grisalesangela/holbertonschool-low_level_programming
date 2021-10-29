abcde11bf737839fd41d6dce2164cfde15f27474
grisalesangela <angelagrisales1@gmail.com>
2021-10-28 22:58:32 -0500

    :test_tube: string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,48 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
 
 /**
  *string_nconcat - concatenates two strings
- *@s1: prefix string
- *@s2: suffix string
- *n: characters to take from s2
- *Return: pointer to a new space contaning s1 and n bytes of n2,
- *ended by a NULL character
+ *@s1: first string
+ *@s2: second string
+ *@n: characters to take from s2
+ *Return: pointer to callocated memory or null if fail
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k, len1, len2;
-	char *ret;
+	unsigned int count1 = 0, count2 = 0; 
+	char *concat, *copy_s;
 
-	len1 = len2 = 0;
 	if (s1 == NULL)
-	{
-		s1 = malloc(1);
-		*s1 = '\0';
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2 = malloc(1);
-		s2 = '\0';
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-		len1 += 1;
-	for (j = 0; s2[j] != '\0'; j++)
-		len2 += 1;
-	if (n > len2)
-		n = len2;
-	ret = malloc((len1 + n) * sizeof(char));
-	if (ret == NULL)
+		s2 = "";
+	
+	while (s1[count1])
+		count1++;
+	while (s2[count2])
+		count2++;
+
+	if (n >= count2)
+		count1 += count2;
+	else
+		count1 += n;
+	
+	concat = malloc(sizeof(char) * (count1 * 1));
+	if (concat == NULL)
 		return (NULL);
-	for (i = 0, k = 0; s1[i] != '\0'; i++, k++)
-		ret[k] = s2[j];
-	ret[k] = '\0'
-	return (ret);
+
+	copy_s = concat;
+	while (*s1)
+		*copy_s++ = *s1++;
+
+	while (n < count1)
+	{
+		*copy_s++ = *s2++;
+		n++;
+	}
+	*copy_s = '\0';
+	return (concat);
 }
+		return(concat);
+	
