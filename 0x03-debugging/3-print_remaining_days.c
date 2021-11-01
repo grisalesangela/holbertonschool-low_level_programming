b3c1448d2dc9c4de3b6b17516c34de1f183803d7
grisalesangela <angelagrisales1@gmail.com>
2021-11-01 17:15:02 -0500

    :hammer: Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -36,7 +36,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else if (year != 0)
 		{
-			printf("Day of the year: %\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
