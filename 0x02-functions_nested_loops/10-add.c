096657132a4a2d77f6d243ec20f54533d4f09282
grisalesangela <angelagrisales1@gmail.com>
2021-11-01 14:49:28 -0500

    :rotating_light: a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - add two integers
+ * @n: first input in the add
+ * @g: second input in the add
+ * Return: add product
+ */
+int add(int n, int g)
+{
+	return (n + g);
+}
