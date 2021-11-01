7660e425d209ae993540dcfae1863f641eb5d4a3
grisalesangela <angelagrisales1@gmail.com>
2021-11-01 17:31:08 -0500

    :poop: isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check for uppercase characters
+ * @c: input character
+ * Return: 1 if the character is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
