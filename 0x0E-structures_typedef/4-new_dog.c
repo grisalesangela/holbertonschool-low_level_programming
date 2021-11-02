cd5c7dd7498aeadbf79181e68b796858b02a52b1
grisalesangela <angelagrisales1@gmail.com>
2021-11-01 23:53:43 -0500

    :triangular_flag_on_post: A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name of the dog
+ * Return: new dog str
+ */
+dog_t *new_dog(char *name,float age, char *owner)
+{
+	dog_t *ndog
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	ndog->name = name;
+	ndog->age = age;
+	ndog->owner = owner;
+	return (ndog);
+}
