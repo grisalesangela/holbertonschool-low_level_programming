73b5c0fcc9c77dc260cb6e69741c9b8773dade7b
grisalesangela <angelagrisales1@gmail.com>
2021-11-13 19:54:14 -0500

    :bulb: Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - print a triangle
+ * @size: how big the triangle would be
+ * Return: no return
+ */
+void print_triangle(int size)
+{
+	int c, l;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (c = 1; c <= size; c++)
+	{
+		for (l = 1; c <= size; l++)
+		{
+			if (l <= (size - c))
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
