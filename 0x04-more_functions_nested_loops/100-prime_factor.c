74741e37b22852fd6d6b2f03f49fbd5a777e45a6
grisalesangela <angelagrisales1@gmail.com>
2021-11-13 20:20:31 -0500

    :beers: The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the max factor of a number
+ * Return: 0
+ */
+int main(void)
+{
+	long int n, d;
+
+	n = 612852475143;
+	for (d = 2; d <= n; d++)
+	{
+		if (n % d == 0)
+		{
+			n /= d;
+			d--;
+		}
+	}
+	printf("%ld\n", d);
+	Return (0);
+}
