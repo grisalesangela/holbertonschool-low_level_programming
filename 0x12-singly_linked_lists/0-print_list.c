8d415e4adbd31a9b5aedcf72bec0fc81b6148a3c
grisalesangela <angelagrisales1@gmail.com>
2021-11-14 23:15:29 -0500

    :test_tube:Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to list to print
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t ret;
+
+	ret = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			ret += 1;
+			continue;
+		}
+		printf("[%i] %s\n", h->len, h->str);
+		ret += 1;
+		h = h->next;
+	}
+	return (ret);
+}
