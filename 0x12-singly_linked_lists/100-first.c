16ce9079398c48b0347ceabead1dbbbcee833020
grisalesangela <angelagrisales1@gmail.com>
2021-11-14 23:58:34 -0500

    :wastebasket:The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * first - print something before main
+ */
+void _attrbute_ ((constructor)) first()
+{
+	char *s;
+
+	s = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+       printf("%s", s);
+}       
