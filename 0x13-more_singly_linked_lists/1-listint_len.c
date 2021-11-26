f61a00517d60cedcdad2836e50249b421f44b0c9
grisalesangela <angelagrisales1@gmail.com>
2021-11-26 13:58:36 -0500

    :coffin: List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,16 @@
 #include "lists.h"
-
 /**
  * listint_len - counts number of nodes of a list_int list
  * @h: head of listint_t list
- * Return: number of nodes
+ * Return: nodes
  */
-
 size_t listint_len(const listint_t *h)
 {
-	unsigned int nodes;
+	int iterador = 0;
 
-	nodes = 0;
-	while (h && h->next != NULL)
+	for (; h; iterador++)
 	{
-		nodes += 1;
 		h = h->next;
 	}
-	if (h)
-		nodes += 1;
-	return (nodes);
+	return (iterador);
 }
