048cbc56d024ac56bdfbff243578b2968626da92
grisalesangela <angelagrisales1@gmail.com>
2021-11-28 22:23:43 -0500

    :clown_face:Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_chessboard - print the chessboard
+ * @a: string to review
+ * Return: no return
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x = 0, y;
+
+	while (x < 9)
+	{
+		y = 0;
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
