a9311a5aa28148095b7629d5aa42e9da6272dda0
grisalesangela <angelagrisales1@gmail.com>
2021-11-28 22:31:43 -0500

    :iphone:The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagsums - print the sum of the two diagonals
+ * @a: pointer t the array
+ * @size: number of rows and columns of the array
+ * Return: no return
+ */
+void print_diagsums(int *a, int size)
+{
+	int f = 0, sum_d1 = 0, sum_d2 = 0;
+
+	while (f < size)
+	{
+		sum_d1 += a[(f * size) - f];
+		f++;
+	}
+	printf("%d, %d\n", sum_d1, sum_d2);
+}
